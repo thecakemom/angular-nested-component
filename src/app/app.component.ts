0f4b5b3f445fce8b9d8e574c172b526707fcbc48
Jennifer Lopez <111323297+thecakemom@users.noreply.github.com>
2023-05-30 14:54:13 -0500

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  myPostsTopLevel = ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5'];
+}
---
+++ src/app/my-profile/my-profile.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-profile',
+  templateUrl: './my-profile.component.html',
+  styleUrls: ['./my-profile.component.css']
+})
+export class MyProfileComponent implements OnInit {
+  @Input () myPostsMidLevel;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
